556989272b1afc589240b26862eed100265e2901
Oskar <oskarlindahl03@gmail.com>
2025-05-22 19:43:10 +0200

    Athor Management done, added get all authors, get by lastname, and create new author

---
+++ src/main/java/com/example/library_system/Controller/Author/GetAuthorsSerchController.java
@@ -0,0 +1,4 @@
+package com.example.library_system.Controller.Author;
+
+public class GetAuthorsSerch {
+}
--- src/main/java/com/example/library_system/Repository/AuthorRepository.java
+++ src/main/java/com/example/library_system/Repository/AuthorRepository.java
@@ -1,5 +1,9 @@
 package com.example.library_system.Repository;
 
-public interface AuthorRepository {
+import com.example.library_system.Entity.Authors;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface AuthorRepository extends JpaRepository<Authors, Long> {
 }
